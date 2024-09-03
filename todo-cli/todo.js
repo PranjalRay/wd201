f62a8b1fedd45e3a32f2ebe61832f6892c09c5d7
Pranjal Ray <roypranjal2015@gmail.com>
2024-09-03 14:57:49 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,1 +1,50 @@
+const todoList = () => {
+  all = []
+  const add = (todoItem) => {
+    all.push(todoItem)
+  }
+  const markAsComplete = (index) => {
+    all[index].completed = true
+  }
+  const formattedDate = d => {
+    return d.toISOString().split("T")[0]
+    }
+  const overdue = () => {
+    const today = new Date().toISOString().split('T')[0]
+    return all.filter(item => !item.completed && item.dueDate < today)
+  }
 
+  const dueToday = () => {
+    const today = new Date();
+    const todayFormatted = formattedDate(today);
+    const dueTodayItems = all.filter((item) => item.dueDate === todayFormatted);
+    return dueTodayItems;
+  }
+
+  const dueLater = () => {
+    const today = new Date().toISOString().split('T')[0]
+    return all.filter(item => !item.completed && item.dueDate > today)
+  }
+
+  const toDisplayableList = (list) => {
+    return list
+      .map(
+        (item, index) =>
+          `${item.completed ? "[x]" : "[ ]"} ${item.title}${
+            item.dueDate === today ? "" : " " + item.dueDate
+          }`
+      )
+      .join("\n");
+  };
+  
+return {
+  all,
+  add,
+  markAsComplete,
+  overdue,
+  dueToday,
+  dueLater,
+  toDisplayableList
+};
+};
+module.exports=todoList
